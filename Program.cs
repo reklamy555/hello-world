c1b698be2502e7522e84acbc46c46f057ee83edf
reklamy555 <reklamy555@gmail.com>
2016-06-20 15:02:03 +0200

    Init commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Linia linia = new Linia();
+
+            Console.Write(linia);
+            Console.ReadKey();
+        }
+    }
+}
