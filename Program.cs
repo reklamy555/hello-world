16c12b901564507b5c1f9db6e1378eabed00bf0a
reklamy555 <reklamy555@gmail.com>
2016-06-20 16:45:39 +0200

    ontur00_1com

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Linia linia = new Linia();
-			Linia ontur00 = new Linia(); ///branch_ontur00_1
+			Linia ontur00 = new Linia(); ///branch_ontur00_1s
             Console.Write(linia);
             Console.ReadKey();
         }
