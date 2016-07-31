44d27d838ecaca8771c6877998596e1239ddcbb4
Artur Garbacik <artur.garbacik@softiq.pl>
2016-07-31 17:51:48 +0200

    Commit reklamy555

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace ConsoleApplication1
         {
             Linia linia = new Linia();
 			Linia ontur00 = new Linia(); ///branch_ontur00_1sssss
+			Linia reklamy555 = new Linia();
             Console.Write(linia);
             Console.ReadKey();
         }
